78dc7a293720c430fdb8083966b20f4de43357e9
ZwolinKox <34248470+ZwolinKox@users.noreply.github.com>
2018-11-28 19:26:30 +0100

    Add files via upload

---
+++ SQL/Passive.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 28 Lis 2018, 19:17
+-- Server version: 5.5.36
+-- PHP Version: 5.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `STGO`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `passive`
+--
+
+CREATE TABLE IF NOT EXISTS `types` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ -- ----------------------------
+ --
+ -- WPISZ TUTAJ WSZYSTKO
+ --
+ -- ----------------------------
+	
+ 
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
