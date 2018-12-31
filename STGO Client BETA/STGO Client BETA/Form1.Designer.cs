8829d9fe879fc3cbfc5ea01b1d5deb7fc0a615ec
Krzychu <krzysztof.zon2001@gmail.com>
2018-12-31 13:26:04 +0100

    STGO Client BETA
    Do tego trzeba usiąść

---
+++ STGO Client BETA/STGO Client BETA/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace STGO_Client_BETA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Browerser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // Browerser
+            // 
+            resources.ApplyResources(this.Browerser, "Browerser");
+            this.Browerser.AllowNavigation = false;
+            this.Browerser.Name = "Browerser";
+            this.Browerser.Url = new System.Uri("http://localhost/STGO/index.php", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Browerser);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser Browerser;
+    }
+}
+
