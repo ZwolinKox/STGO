043adef2fa478368aee0f2f3eb9e908d4f47bf8e
ZwolinKox <zwolinn@wp.pl>
2019-01-03 14:02:48 +0100

    Dodanie spania, do uzupełnienia jak chodzi o efekty spania

--- template.php
+++ template.php
@@ -57,10 +57,10 @@
 				
 				<div class="col-12 col-md-6" style="margin-top: 15px;">
 
-                        <div class="btn-dark btn-lg href" id="https://www.onet.pl">1. Idz do szkoly (Z wyjatkiem weekendu, raz dziennie)</div>
-                        <div class="btn-dark btn-lg href-blank" id="https://www.onet.pl">2. Idz do sklepu</div>
+                        <div class="btn-dark btn-lg href" id="https://www.mechanik.rac.pl">1. Idz do szkoly (Z wyjatkiem weekendu, raz dziennie)</div>
+                        <div class="btn-dark btn-lg href-blank" id="https://www.allegro.pl">2. Idz do sklepu</div>
                         <div class="btn-dark btn-lg href" id="napraw-komputer.php">3. Napraw komputer</div>
-                        <div class="btn-dark btn-lg">4. Idz spac</div>
+                        <div class="btn-dark btn-lg href" id="spanie.php">4. Idz spac</div>
                         <div class="btn-dark btn-lg href" id="silownia.php">5. Idz wreszcie na silownie</div>
                         <div class="btn-dark btn-lg href" id="memy.php">6. Wyslij mema na grupe</div>
                         <div class="btn-dark btn-lg href" id="brat.php">7. Wyzywanie brata</div>
