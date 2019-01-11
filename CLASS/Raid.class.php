fa4bb2de7ceba8fe6f594e3057ff4803d1bd3371
Krzychu <krzysztof.zon2001@gmail.com>
2019-01-11 18:31:24 +0100

    Update Raid.class.php

---
+++ CLASS/Raid.class.php
@@ -0,0 +1,28 @@
+<?php
+
+    class Raid{
+
+        //Domyslne rzeczy które sie wykonuja na kazdym raidzie
+        //ta metoda jest wykonywana w kazdej metodzie kazdego raidu
+        public static function setUp(){
+
+        }
+        //Ostrog na level 15
+        public static function raidOstrog(){
+
+        }
+        //Markowice na level 20
+        public static function raidMarkowice(){
+
+        }
+        //Babice na level 25
+        public static function raidBabice(){
+
+        }
+        //Nędza na level 30
+        public static function raidNedza(){
+
+        }
+    }
+
+?>
