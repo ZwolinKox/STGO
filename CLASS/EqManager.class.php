f62ced604fd3a793d32331e15da351349eaa4c16
Krzychu <krzysztof.zon2001@gmail.com>
2019-01-18 21:55:46 +0100

    Dodanie klasy EqManager oraz poprawa BuyById()

---
+++ CLASS/EqManager.class.php
@@ -0,0 +1,72 @@
+<?php
+
+//Klasa która pomoże nam zarządzać ewkipunkiem
+class EqManager {
+    //Szukanie pierwszego wolnego miejsca w ekwipunku
+    //W przyszłości tą metode zastosujemy w klasie TradeManager w metodzie BuyById, narazie niech służy tylko do zarządzanie eq
+    public static function findSpace() {
+        if(DatabaseManager::selectBySQL("SELECT eqSlotOne FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotOne'] == 0)
+        {
+            return 'eqSlotOne';
+        }
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotTwo FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotTwo'] == 0)
+        {
+            return 'eqSlotTwo';
+        }
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotThree FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotThree'] == 0)
+        {
+            return 'eqSlotThree';
+        }
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotFour FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotFour'] == 0)
+        {
+            return 'eqSlotFour';
+        }
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotFive FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotFive'] == 0)
+        {
+            return 'eqSlotFive';
+        }
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotSix FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotSix'] == 0)
+        {
+            return 'eqSlotSix';
+        }     
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotSeven FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotSeven'] == 0)
+        {
+            return 'eqSlotSeven';
+        }
+        else if(DatabaseManager::selectBySQL("SELECT eqSlotEight FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotEight'] == 0)
+        {
+            return 'eqSlotEight';
+        }
+    }
+
+    //Sprawdzanie czy możemy używać daną broń
+    //Ta metoda będzie wywoływana za każym razem jak będziemy ubierać broń, jeśli zwróci true to warunek będzie spełnmiony i zsotanie ubrana, jeśli nie to nie
+    public static function checkHand($itemid) {
+        if(DatabaseManager::selectBySQL("SELECT forLevel FROM items WHERE id=".$itemid)[0]['forLevel'] > DatabaseManager::selectBySQL("SELECT userLevel FROM users WHERE id=".$_SESSION['uid'])[0]['userLevel'])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //Musze na spokojnie przeanalizować czy to ma sens ale chyba tak
+    //Zakładanie danego przedmiotu
+    public static function putOn($slot)
+    {
+        if(checkHand(DatabaseManager::selectBySQL("SELECT '.$slot.' FROM users WHERE id=".$_SESSION['uid'])[0][$slot]))
+        {
+            $bufferOld = DatabaseManager::selectBySQL("SELECT eqMainHand FROM users WHERE id=".$_SESSION['uid'])[0]['eqMainHand'];
+            $bufferNew = DatabaseManager::selectBySQL("SELECT '.$slot.' FROM users WHERE id=".$_SESSION['uid'])[0][$slot];
+
+            DatabaseManager::updateTable('users', ['eqMainHand' => $bufferNew, $slot => $bufferOld], ['id' => $_SESSION['uid']]);
+
+            unset($bufferNew, $bufferOld);
+        }
+    }
+
+}
+
+?>
--- CLASS/TradeManager.class.php
+++ CLASS/TradeManager.class.php
@@ -5,49 +5,63 @@ class TradeManager {
     static public function buyById($id) {
         if(DatabaseManager::selectbySQL("SELECT slyszCoin FROM users WHERE id=".$_SESSION['uid'])[0]['slyszCoin'] > DatabaseManager::selectBySQL("SELECT vendorCost FROM items WHERE id=$id")[0]['vendorCost'])
         {
-            $cost = DatabaseManager::selectBySQL("SELECT vendorCost FROM items WHERE id=$id")[0]['vendorCost'];
-            DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
-            unset($cost);
+            if(DatabaseManager::selectBySQL("SELECT vendorCost FROM items WHERE id=$id")[0]['vendorCost'] != 0)
+            {
+                $cost = DatabaseManager::selectBySQL("SELECT vendorCost FROM items WHERE id=$id")[0]['vendorCost'];
+                
+                if(DatabaseManager::selectBySQL("SELECT eqSlotOne FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotOne'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotOne' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotTwo FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotTwo'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotTwo' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotThree FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotThree'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotThree' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotFour FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotFour'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotFour' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotFive FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotFive'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotFive' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotSix FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotSix'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotSix' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotSeven FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotSeven'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotSeven' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else if(DatabaseManager::selectBySQL("SELECT eqSlotEight FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotEight'] == 0)
+                {
+                    DatabaseManager::updateTable('users', ['eqSlotEight' => $id], ['id' => $_SESSION['uid']]);
+                    DatabaseManager::updateTable('users', ['slyszCoin' => 'slyszCoin-'.$cost], ['id' => $_SESSION['uid']]);
+                }
+                else
+                {
+                    echo '<h3 style="color: red;">Nie masz miejsca w ekwipunku!</h3>';
+                }
 
-            if(DatabaseManager::selectBySQL("SELECT eqMainHand FROM users WHERE id=".$_SESSION['uid'])[0]['eqMainHand'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqMainHand' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotOne FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotOne'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotOne' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotTwo FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotTwo'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotTwo' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotThree FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotThree'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotThree' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotFour FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotFour'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotFour' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotFive FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotFive'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotFive' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotSix FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotSix'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotSix' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotSeven FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotSeven'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotSeven' => $id], ['id' => $_SESSION['uid']]);
-            }
-            else if(DatabaseManager::selectBySQL("SELECT eqSlotEight FROM users WHERE id=".$_SESSION['uid'])[0]['eqSlotEight'] == 0)
-            {
-                DatabaseManager::updateTable('users', ['eqSlotEight' => $id], ['id' => $_SESSION['uid']]);
+                if(isset($cost))
+                {
+                    unset($cost);
+                }
             }
             else
             {
-                echo '<h3 style="color: red;">Nie masz miejsca w ekwipunku!</h3>';
+                echo '<h3 style="color: red;">Nie możesz kupić tego przedmiotu!</h3>'; 
             }
         }
         else
