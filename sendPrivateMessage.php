09624219b614c77c232827cda55d02ba8fc54ac5
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2019-03-13 15:35:42 +0100

    Usuniece buga z tagami html w prywatnych wiadomosciach

--- sendPrivateMessage.php
+++ sendPrivateMessage.php
@@ -1,7 +1,7 @@
 <?php
     require_once('config.php');
 
-    GameMail::sendMail(Post::get('adresat'), $_SESSION['uid'], Post::get('tresc'));
+    GameMail::sendMail(Post::get('adresat'), $_SESSION['uid'], strip_tags(Post::get('tresc')));
     URL::to("poczta.php");
-  
+
 ?>
