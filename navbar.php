560f7f70d52ffbf4f348ff7fde907d45ae59a5d7
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2019-05-19 17:01:27 +0200

    Naprawa buga z navbar oraz linki w rankingu!

--- navbar.php
+++ navbar.php
@@ -1,7 +1,15 @@
 <?php
-$nick = DatabaseManager::selectBySQL("SELECT username FROM users WHERE id=".$_SESSION['uid'])[0]['username'];
 
-echo '<div class="col-4 btn btn-dark btn-lg href" id="ksiazka.php?profile='.$nick.'">Profil</div>';
+if(isset($_SESSION['logged']) && $_SESSION['logged'] == true)
+{
+    $nick = DatabaseManager::selectBySQL("SELECT username FROM users WHERE id=".$_SESSION['uid'])[0]['username'];
+    echo '<div class="col-4 btn btn-dark btn-lg href" id="ksiazka.php?profile='.$nick.'">Profil</div>';
+}
+else
+{
+    echo '<div class="col-4 btn btn-dark btn-lg">Profil</div>';
+}
+
 echo '<div class="col-4 btn btn-dark btn-lg href" id="ranking.php">Ranking</div>';
 echo '<div class="col-4 btn btn-dark btn-lg href" id="logout.php">Wyloguj</div>';
 ?>
