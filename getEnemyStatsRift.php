f575dfb101ce371425375cb5e6ce6a5d795d1cc6
Krzysztofz01 <krzysztof.zon2001@gmail.com>
2019-06-11 17:08:37 +0200

    Rifty - need to debug

--- getEnemyStatsRift.php
+++ getEnemyStatsRift.php
@@ -38,57 +38,31 @@ elseif ($_POST['co'] == 'Cios') {
     $playerDmg = ($playerStats['statStrength'] / 100 ) * $playerWeapon['addStrenght'] + ($playerStats['statIntelect'] / 100 ) * $playerWeapon['addIntelect'] + $playerWeapon['addDamage'];
 
     if($riftEnemyStat['hp'] > 0) {
-        $_SESSION['enemyInfo']['enemyArmor'] -= $playerDmg;
+        $riftEnemyStat['armor'] -= $playerDmg;
 
-        if($_SESSION['enemyInfo']['enemyArmor'] < 0)
-            $_SESSION['enemyInfo']['enemyArmor'] = 0;
+        if($riftEnemyStat['armor'] < 0)
+            $riftEnemyStat['armor'] = 0;
 
     } else {
-        $_SESSION['enemyInfo']['enemyHp'] -= $playerDmg;
+        $riftEnemyStat['hp'] -= $playerDmg;
     }
 
     DatabaseManager::updateTable('users', ['statHp' => "statHp-$enemyDmg"], ['id' => $_SESSION['uid']]);
 
+    $_SESSION['riftEnemy'] = serialize($riftEnemy);
+
     if(DatabaseManager::selectBySQL("SELECT statHp FROM users WHERE id=".$_SESSION['uid'])[0]['statHp'] <= 0) {
         UserManager::death();
     }
-    elseif ($_SESSION['enemyInfo']['enemyHp'] <= 0) {
+    elseif ($riftEnemyStat['hp'] <= 0) {
 
         if(!$_SESSION['winWithMonster'])
         {
-            $_SESSION['enemyInfo']['enemyHp'] = 0;
+            $riftEnemyStat['hp'] = 0;
             $_SESSION['winWithMonster'] = true;
     
-    
-            DatabaseManager::updateTable('users', ['xpPoints' => "xpPoints+".$_SESSION['enemyInfo']['dropXp'], 'slyszCoin' => 'slyszCoin+'.$_SESSION['enemyInfo']['dropSlyszCoin']], ['id' => $_SESSION['uid']]);
-            
-            if(DatabaseManager::selectBySQL("SELECT xpPoints FROM users WHERE id=".$_SESSION['uid'])[0]['xpPoints']
-                >= DatabaseManager::selectBySQL("SELECT maxXp FROM users WHERE id=".$_SESSION['uid'])[0]['maxXp']) 
-            {
-                
-                Drop::levelUp(DatabaseManager::selectBySQL('SELECT userLevel FROM users WHERE id='.$_SESSION['uid'])[0]['userLevel']);
-                $_SESSION['lvlup'] = true;
-            }
-    
-            //Losowanie, czy będzie drop
-            $itemDrop = Drop::school(1);
-            if($itemDrop != null)
-            {
-                $freeSlot = EqManager::findSpace();
-                
-                if($freeSlot != null) 
-                {
-                    DatabaseManager::updateTable('users', [$freeSlot => $itemDrop], ['id' => $_SESSION['uid']]);
-                }
-                else
-                {
-                    $_SESSION['enemyInfo']['isFull'] = true;
-                }
-    
-                $_SESSION['enemyInfo']['dropItem'] = $itemDrop;
-            
-            }
-    
+            //Tutaj daj kod przejscie do nastepnego przeciwnika
+
             $_SESSION['fight'] = false;
             unset($_SESSION['fight']);
       
@@ -96,12 +70,10 @@ elseif ($_POST['co'] == 'Cios') {
     
             die('win');
         }
-
     }
         
-    
     else {
-        if($_SESSION['enemyInfo']['enemyHp'])
+        if($riftEnemyStat['hp'])
 
         die("<div class='alert alert-warning alert-dismissible fade show' role='alert'>
         Przeciwnik zadał Ci obrażenia równe $enemyDmg
